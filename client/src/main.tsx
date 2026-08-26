4240d6f76449ebc88482ed4ad9cb0c5b4de68508
Alwinriyas <alwinriyas16@gmail.com>
2026-08-26 14:18:54 +0530

    refactor: remove legacy navigation bridge

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,5 @@ import './design-system.css';
 import './product-ui.css';
 import './navigation.css';
 import App from './App';
-import { installProjectFlowNavigation } from './navigation-bridge';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
-installProjectFlowNavigation();
