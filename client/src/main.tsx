bf6ababa2a49cf8ba52bc80c395f779f85e4b61e
Alwinriyas <alwinriyas16@gmail.com>
2026-08-26 14:18:39 +0530

    style: load navigation styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './styles.css';
 import './design-system.css';
 import './product-ui.css';
+import './navigation.css';
 import App from './App';
 import { installProjectFlowNavigation } from './navigation-bridge';
 
